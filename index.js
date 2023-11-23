d7c646c6ee851d97c799f918d5d88c9fa0ff628d
Mukul <mukul1singh23@gmail.com>
2023-11-24 00:16:54 +0530

    login signup added

--- index.js
+++ index.js
@@ -2,15 +2,91 @@
 const express = require("express");
 const app=express();
 const path=require('path');
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded({extended:false}))
 
 app.use(express.static(path.join(__dirname,'public')))
 app.set("view engine",'ejs');
 app.set('views',path.join(__dirname,'views'));
+//mongoose connect
+
+mongoose
+  .connect(
+    "mongodb+srv://shapeShiftDB:mukulbhai16@cluster0.vmp43iw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("mongoose connected");
+  })
+  .catch((e) => {
+    console.log("failed");
+  });
+
+
+  //schema
+
+  const authentication = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    pass: {
+      type: String,
+      required: true,
+    },
+  });
+
+
+
+  const auth = mongoose.model("auth", authentication);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const data = new auth({
+      name: req.body.username,
+      email: req.body.email,
+      pass: req.body.password,
+    });
+    // console.log(data.username);
+    // console.log(data);
+    await data.save();
+  } catch (err) {
+    console.log(err);
+  }
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const check = await auth.findOne({ email: req.body.email });
+    if (check.pass === req.body.password) {
+      res.render("index");
+    } else {
+      res.send("incorrect password");
+    }
+  } catch (e) {
+    res.render("index");
+    // res.send("Something Went Wrong");
+  }
+});
+
+
+
+
+
+
+
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 })
 app.get('/index',(req,res)=>{
     res.render('index')
