b647fef990d77c4892ec5cc78c9c07107fc7cbf9
Mukul <mukul1singh23@gmail.com>
2023-12-01 20:41:14 +0530

    pic added

--- public/js/bmiscript.js
+++ public/js/bmiscript.js
@@ -1,9 +1,9 @@
-let button = document.getElementById('btn');
+let button = document.getElementById('bmi');
 
 button.addEventListener('click', () => {
     const height = parseInt(document.getElementById('height').value);
     const weight = parseInt(document.getElementById('weight').value);
-    const result = document.getElementById('output');
+    const result = document.getElementById('output-bmi');
     let height_status=false, weight_status=false;
 
     if(height === '' || isNaN(height) || (height <= 0)){
@@ -30,6 +30,32 @@ button.addEventListener('click', () => {
         }else{
             result.innerHTML = 'Over weight : ' + bmi;
         }
+    }else{
+        alert('The form has errors');
+        result.innerHTML = '';
+    }
+});
+
+// daily protine intake need  js
+let button2 = document.getElementById('pin');
+
+button2.addEventListener('click', () => {
+    const weight = parseInt(document.getElementById('weight-pin').value);
+    const result = document.getElementById('output-pin');
+    let  weight_status=false;
+
+    
+
+    if(weight === '' || isNaN(weight) || (weight <= 0)){
+        document.getElementById('weight_error').innerHTML = 'Please provide a valid weight';
+    }else{
+        document.getElementById('weight_error').innerHTML = '';
+        weight_status=true;
+    }
+
+    if(weight_status){
+        const pinc = (weight * 1.5).toFixed(2);
+        result.innerHTML = 'your daily protein intake need is : ' + pinc +' grams';
     }else{
         alert('The form has errors');
         result.innerHTML = '';
